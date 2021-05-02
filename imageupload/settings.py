a95fd6757e5fd488078d37ee6a51f7e093853479
nihallondhe <79393628+nihallondhe@users.noreply.github.com>
2021-05-02 15:49:56 +0530

    Update settings.py

--- imageupload/settings.py
+++ imageupload/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_2^wazua3t!i0kio4wmsf_-(cf93j6i=o^c=1xjpgk84kr4^i1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
